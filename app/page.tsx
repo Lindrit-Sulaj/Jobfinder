94924e1d3337d5ceb69b000580662e6527d15d04
Lindrit <lindritsulaj2.0@gmail.com>
2023-07-06 14:26:20 +0200

    No API

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { Search } from '@/components'
 
 async function getJobs() {
-  const res = await fetch('http://localhost:3000/api/jobs');
+  const res = await fetch('http://localhost:3000/api/jobs', {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
   if (!res.ok) {
     throw new Error("Couldn't fetch")
   }
@@ -11,12 +15,12 @@ async function getJobs() {
 }
 
 export default async function Home() {
-  const jobs = await getJobs();
+  // const jobs = await getJobs();
 
   return (
     <main className='px-6 md:px-8'>
       <Search />
-      <pre>{ JSON.stringify(jobs, null, 2)}</pre>
+      {/* <pre>{ JSON.stringify(jobs, null, 2)}</pre> */}
       <span>{new Date().getTime()}</span>
     </main> 
   )
