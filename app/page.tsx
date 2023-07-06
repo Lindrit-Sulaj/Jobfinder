4ebe8fecb8b1f99d265dda0767e108f7c8acbe78
Lindrit <lindritsulaj2.0@gmail.com>
2023-07-06 14:30:49 +0200

    With API

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,12 @@ async function getJobs() {
 }
 
 export default async function Home() {
-  // const jobs = await getJobs();
+  const jobs = await getJobs();
 
   return (
     <main className='px-6 md:px-8'>
       <Search />
-      {/* <pre>{ JSON.stringify(jobs, null, 2)}</pre> */}
+      <pre>{ JSON.stringify(jobs, null, 2)}</pre>
       <span>{new Date().getTime()}</span>
     </main> 
   )
